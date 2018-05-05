dc4df2b7d25a91d61cb994be1b6de88730f6ddc8
matsmaker <matsitskyss@gmail.com>
2018-05-05 22:53:47 +0300

    update example

--- example/main.js
+++ example/main.js
@@ -1,2 +1,2 @@
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
-document.body.appendChild(app.view);
+var pixiApplication = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+document.body.appendChild(pixiApplication.view);
