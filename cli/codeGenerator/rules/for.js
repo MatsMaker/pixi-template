f6d213c18a2ec59c04bd57a8f131b8a480d5ec99
matsmaker <matsitskyss@gmail.com>
2018-05-13 23:41:25 +0300

    add expression in for rules

--- cli/codeGenerator/rules/for.js
+++ cli/codeGenerator/rules/for.js
@@ -4,11 +4,18 @@ module.exports = function(node, nIndex) {
     const counter = parseInt(node.rules[':for']);
 
     node.parent.children.splice(nIndex, 1);
-    const newNode = _.cloneDeep(node);
-    newNode.parent = node.parent;
-    delete newNode.rules[':for'];
 
-    _.forEach(_.range(counter), index => {
-        node.parent.children.splice(nIndex + index, 0, newNode);
+    _.forEach(_.range(counter), key => {
+        const newNode = _.cloneDeep(node);
+        newNode.parent = node.parent;
+        delete newNode.rules[':for'];
+        _.forEach(newNode.property, (pValue, pKey) => {
+            const expression = pValue.match(/({{)(.*)(}})/);
+            if (!_.isNull(expression)) {
+                const res = eval(expression[2]);
+                newNode.property[pKey] = pValue.replace(expression[0], res);
+            }
+        });
+        node.parent.children.splice(nIndex + key, 0, newNode);
     });
 }
